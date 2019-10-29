bfb73195a7dc1a480bb356f7ee77d652658d6d24
dfsanchezg <dfsg@outlook.com>
2019-10-29 12:25:49 -0500

    add the new project in spring boot

---
+++ src/main/java/com/merlin/bicycle/bicycleMerlin/BicycleMerlinApplication.java
@@ -0,0 +1,13 @@
+package com.merlin.bicycle.bicycleMerlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleMerlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicycleMerlinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/merlin/bicycle/bicycleMerlin/BicycleMerlinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merlin.bicycle.bicycleMerlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicycleMerlinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
